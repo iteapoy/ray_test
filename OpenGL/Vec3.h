7f9e69a0b3cae8866a82f0475c26ee63aae8661b
iteapoy <44200228+iteapoy@users.noreply.github.com>
2018-12-29 13:58:44 +0800

    init

---
+++ OpenGL/Vec3.h
@@ -0,0 +1,55 @@
+#pragma once
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <fstream>
+#include <vector>
+#include <iostream>
+#include <cassert>
+#include <algorithm>
+#include <sstream>
+
+using namespace std;
+
+#ifndef VEC3_H
+#define VEC3_H
+
+// 3维向量
+template<typename T>
+class Vec3
+{
+public:
+	T x, y, z;
+	Vec3() : x(T(0)), y(T(0)), z(T(0)) {}
+	Vec3(T xx) : x(xx), y(xx), z(xx) {}
+	Vec3(T xx, T yy, T zz) : x(xx), y(yy), z(zz) {}
+
+	// 向量归一化
+	Vec3& normalize()
+	{
+		T nor2 = length2();
+		if (nor2 > 0) {
+			T invNor = 1 / sqrt(nor2);
+			x *= invNor, y *= invNor, z *= invNor;
+		}
+		return *this;
+	}
+
+	Vec3<T> operator * (const T &f) const { return Vec3<T>(x * f, y * f, z * f); }
+	Vec3<T> operator * (const Vec3<T> &v) const { return Vec3<T>(x * v.x, y * v.y, z * v.z); }
+	T dot(const Vec3<T> &v) const { return x * v.x + y * v.y + z * v.z; }
+	Vec3<T> operator - (const Vec3<T> &v) const { return Vec3<T>(x - v.x, y - v.y, z - v.z); }
+	Vec3<T> operator + (const Vec3<T> &v) const { return Vec3<T>(x + v.x, y + v.y, z + v.z); }
+	Vec3<T>& operator += (const Vec3<T> &v) { x += v.x, y += v.y, z += v.z; return *this; }
+	Vec3<T>& operator *= (const Vec3<T> &v) { x *= v.x, y *= v.y, z *= v.z; return *this; }
+	Vec3<T> operator - () const { return Vec3<T>(-x, -y, -z); }
+	T length2() const { return x * x + y * y + z * z; }
+	T length() const { return sqrt(length2()); }
+	friend ostream & operator << (ostream &os, const Vec3<T> &v)
+	{
+		os << "[" << v.x << " " << v.y << " " << v.z << "]";
+		return os;
+	}
+};
+
+#endif
