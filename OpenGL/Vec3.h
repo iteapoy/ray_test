9b550a43d7acae37beb1964f14c6c6f1a331f3d3
iteapoy <44200228+iteapoy@users.noreply.github.com>
2018-12-29 14:47:01 +0800

    增加点乘叉乘

--- OpenGL/Vec3.h
+++ OpenGL/Vec3.h
@@ -27,7 +27,7 @@ public:
 	// 向量归一化
 	Vec3& normalize()
 	{
-		T nor2 = length2();
+		T nor2 = sqrLength();
 		if (nor2 > 0) {
 			T invNor = 1 / sqrt(nor2);
 			x *= invNor, y *= invNor, z *= invNor;
@@ -37,14 +37,21 @@ public:
 
 	Vec3<T> operator * (const T &f) const { return Vec3<T>(x * f, y * f, z * f); }
 	Vec3<T> operator * (const Vec3<T> &v) const { return Vec3<T>(x * v.x, y * v.y, z * v.z); }
+	// 叉乘
+	T crossMul(const Vec3<T> &v) const {return Vec3<T>(-z * v.y + y * v.z, z * v.x - x * v.z,-y * v.x + x * v.y);}
+	// 点乘
 	T dot(const Vec3<T> &v) const { return x * v.x + y * v.y + z * v.z; }
+
 	Vec3<T> operator - (const Vec3<T> &v) const { return Vec3<T>(x - v.x, y - v.y, z - v.z); }
 	Vec3<T> operator + (const Vec3<T> &v) const { return Vec3<T>(x + v.x, y + v.y, z + v.z); }
 	Vec3<T>& operator += (const Vec3<T> &v) { x += v.x, y += v.y, z += v.z; return *this; }
 	Vec3<T>& operator *= (const Vec3<T> &v) { x *= v.x, y *= v.y, z *= v.z; return *this; }
 	Vec3<T> operator - () const { return Vec3<T>(-x, -y, -z); }
-	T length2() const { return x * x + y * y + z * z; }
-	T length() const { return sqrt(length2()); }
+
+	// 长度的平方
+	T sqrLength() const { return x * x + y * y + z * z; }
+	// 长度
+	T length() const { return sqrt(sqrLength()); }
 	friend ostream & operator << (ostream &os, const Vec3<T> &v)
 	{
 		os << "[" << v.x << " " << v.y << " " << v.z << "]";
