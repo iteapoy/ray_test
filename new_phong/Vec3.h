45416a0d00b8021a34faba03df69523ae49188c0
iteapoy <44200228+iteapoy@users.noreply.github.com>
2019-01-07 15:24:42 +0800

    一个能显示图像的画布

---
+++ new_phong/Vec3.h
@@ -0,0 +1,54 @@
+#pragma once
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+#ifndef VEC3_H
+#define VEC3_H
+
+//定义了3维向量的加、减、乘、除、点乘、叉乘、输出、归一化
+class Vec3f
+{
+public:
+	float x, y, z;
+	Vec3f() : x(float(0)), y(float(0)), z(float(0)) {}
+	Vec3f(float xx) : x(xx), y(xx), z(xx) {}
+	Vec3f(float xx, float yy, float zz) : x(xx), y(yy), z(zz) {}
+
+	Vec3f operator * (const float &f) const { return Vec3f(x * f, y * f, z * f); }
+	Vec3f operator * (const Vec3f &v) const { return Vec3f(x * v.x, y * v.y, z * v.z); }
+	// 叉乘
+	Vec3f cross(const Vec3f &v) const {return Vec3f(-z * v.y + y * v.z, z * v.x - x * v.z,-y * v.x + x * v.y);}
+	// 点乘
+	float dot(const Vec3f &v) const { return x * v.x + y * v.y + z * v.z; }
+
+	Vec3f operator - (const Vec3f &v) const { return Vec3f(x - v.x, y - v.y, z - v.z); }
+	Vec3f operator + (const Vec3f &v) const { return Vec3f(x + v.x, y + v.y, z + v.z); }
+	Vec3f& operator += (const Vec3f &v) { x += v.x, y += v.y, z += v.z; return *this; }
+	Vec3f& operator *= (const Vec3f &v) { x *= v.x, y *= v.y, z *= v.z; return *this; }
+	Vec3f operator - () const { return Vec3f(-x, -y, -z); }
+
+	// 长度的平方
+	float sqrLength() const { return x * x + y * y + z * z; }
+	// 长度
+	float length() const { return sqrt(sqrLength()); }
+
+	// 向量归一化
+	Vec3f normalize()
+	{
+		float length = this->length();
+		if (length == 0) return Vec3f(0, 0, 0);
+		x = x / length;
+		y = y / length;
+		z = z / length;
+		return Vec3f(x, y, z);
+	}
+	friend ostream & operator << (ostream &os, const Vec3f &v)
+	{
+		os << "[" << v.x << " " << v.y << " " << v.z << "]";
+		return os;
+	}
+};
+
+#endif
---
+++ new_phong/main.cpp
@@ -0,0 +1,88 @@
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <fstream>
+#include <vector>
+#include <iostream>
+#include <cassert>
+#include <algorithm>
+#include <sstream>
+
+#define GLEW_STATIC
+#define GLFW_INCLUDE_GLU
+#include <GL\glew.h>
+#include <GLFW\glfw3.h>
+#include <glm\vec3.hpp>
+#include <glm\glm.hpp>
+
+#include "Vec3.h"
+
+using namespace std;
+
+// 画布大小
+const GLuint WIDTH = 800, HEIGHT = 800;
+
+long maxDepth = 20;
+float dD = 255.0f / maxDepth;
+float dx = 1.0f / WIDTH;
+float dy = 1.0f / HEIGHT;
+
+// 递归深度
+#define MAX_RAY_DEPTH 3
+
+void key_call_back(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+int main(int argc, char **argv)
+{
+	// 窗口初始化
+	glfwInit();
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Ray Tracing", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		cout << "Failed to create GLFW window" << endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	//在窗口中渲染
+	glfwSetKeyCallback(window, key_call_back);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents(); //响应事件
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glLoadIdentity();
+		glScaled(2.0f, 2.0f, 1.0f);
+		glTranslatef(-0.5f, -0.5f, 1.0f);
+
+		glBegin(GL_POINTS);
+		for (long y = 0; y < HEIGHT; ++y)
+		{
+			float sy = 1 - dy * y;
+			for (long x = 0; x < WIDTH; ++x)
+			{
+				float sx = dx * x;
+
+				//glColor3ub(color.r * 255, color.g * 255, color.b * 255);
+				glColor3ub(sx * 255, sy * 255, 0.2);
+				glVertex2f(sx, sy);
+			}
+		}
+		glEnd();
+
+		glfwSwapBuffers(window); // 交换缓存
+	}
+
+	glfwTerminate();
+
+	system("pause");
+	return 0;
+}
+
+void key_call_back(GLFWwindow* window, int key, int scancode, int action, int mode) {
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
